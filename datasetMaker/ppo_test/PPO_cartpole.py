ec5800e62a1ef6711f1b604dda5f7718228c286b
TeddyElectronics <kyc20126@gmail.com>
2025-07-30 17:55:02 +0900

    Add PPO training scripts and expert data collection
    Added new scripts for PPO training on Reacher and CartPole environments, including parallel environment support and success rate logging. Introduced expert trajectory collection for Reacher. Added model checkpoint zip files for CartPole, FrozenLake, and Reacher. Minor cleanup in PPO_example.py and added placeholder for ppo_hc.py.

---
+++ datasetMaker/ppo_test/PPO_cartpole.py
@@ -0,0 +1,33 @@
+import gymnasium as gym
+import numpy as np
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+n_envs = 50
+
+# Parallel environments
+vec_env = make_vec_env("CartPole-v1", n_envs=n_envs)
+
+model = PPO("MlpPolicy", vec_env, verbose=1, device="cpu")
+model.learn(total_timesteps=50000, progress_bar=True)
+model.save("ppo_test_CartPole")
+
+del model # remove to demonstrate saving and loading
+
+model = PPO.load("ppo_test_CartPole")
+
+ep_cnt=0
+success_cnt=0
+
+obs = vec_env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = vec_env.step(action)
+    vec_env.render("human")
+    if dones.any():
+        ep_cnt += dones.sum()
+        success_cnt += (rewards[dones].sum()>0)
+        
+        if ep_cnt%100==0:
+            print(f'success rate = {success_cnt/ep_cnt} ({success_cnt}/{ep_cnt})')
